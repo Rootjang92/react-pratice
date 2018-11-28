d5fd473d2f702fc764eca9712c540f383b59b16a
Rootjang92 <janggeunho7@gmail.com>
2018-11-28 15:19:41 +0900

    React basic tutorials

---
+++ todoapp/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddForm from './AddForm';
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {id:1, content: 'buy some milk'},
+      {id:2, content: 'play the battleground'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.floor(Math.random());
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="Todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <AddForm addTodo={this.addTodo}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    );
+  }
+}
+
+export default App;
